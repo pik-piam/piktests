94a923de7a89b6bee92851be498152480c6a6d46
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-10-14 18:48:03 +0200

    add use slurm

--- R/runInRenv.R
+++ R/runInRenv.R
@@ -3,12 +3,15 @@
 #' Creates a test run folder, sets up a fresh renv and installs needed packages in it. Then calls piktests:::run() to
 #' run the actual tests.
 #'
+#' @param useSlurm Whether to start the tests via sbatch (default, run in background) or directly in the current shell.
+#'
 #' @importFrom madrat getConfig
 #' @importFrom utils packageDescription
 #' @importFrom withr local_dir
 #' @export
-runInRenv <- function() { # TODO submit to slurm arg
-  runFolder <- file.path(getwd(), format(Sys.time(), "%Y_%m_%d-%H_%M"))
+runInRenv <- function(useSlurm = TRUE) {
+  now <- format(Sys.time(), "%Y_%m_%d-%H_%M")
+  runFolder <- file.path(getwd(), now)
   if (file.exists(runFolder)) {
     stop(runFolder, " already exists!")
   }
@@ -18,10 +21,25 @@ runInRenv <- function() { # TODO submit to slurm arg
   getConfig()
   saveRDS(getOption("madrat_cfg"), "initialMadratConfig.rds")
 
-  # copying the piktests DESCRIPTION so renv can automatically determine dependencies
-  file.copy(attr(packageDescription("piktests"), "file"), runFolder)
+  # this is executed in the new renv
+  writeLines(c("renv::record('pfuehrlich-pik/piktests')",
+               "renv::restore()",
+               # "renv::install('~/dev/piktests'); ", # for local development/testing
+               "piktests:::run()"),
+             "installDependenciesAndRun.R")
+
+  # TODO this crashes, but running renv::init() in an interactive R session works
   system2("Rscript", "-", input = "renv::init()")
-  # TODO comment in next line, remove copying DESCRIPTION, submit to slurm, write output to logfile
-  # system2("Rscript", "-", input = "renv::record('piktests'); renv::restore(); renv::snapshot()")
-  system2("Rscript", "-", input = "renv::restore(); renv::install('/home/pascal/dev/piktests'); piktests:::run()")
+
+  logFile <- "runInRenv.log"
+  if (useSlurm) {
+    system2("sbatch", c(paste0("--job-name=piktests-", now),
+                        paste0("--output=", logFile),
+                        "--mail-type=END",
+                        "--qos=priority",
+                        "--mem=32000",
+                        paste0("--wrap=", shQuote("Rscript installDependenciesAndRun.R"))))
+  } else {
+    system2("Rscript", "installDependenciesAndRun.R", stdout = logFile, stderr = logFile, wait = FALSE)
+  }
 }
