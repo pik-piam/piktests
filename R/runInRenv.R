3f3cf421001d9770dab8caa459d5383e9867b69f
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-12-07 14:45:41 +0100

    renvInstallPackages argument

--- R/runInRenv.R
+++ R/runInRenv.R
@@ -3,13 +3,15 @@
 #' Creates a test run folder, sets up a fresh renv and installs needed packages in it. Then calls piktests:::run() to
 #' run the actual tests.
 #'
+#' @param renvInstallPackages Which packages should be installed via renv::install after installing other packages.
+#' Use this to test changes in your fork by passing "<gituser>/<repo>" (e.g. "pfuehrlich-pik/madrat").
 #' @param useSbatch Whether to start the tests via sbatch (run in background) or directly in the current shell. If NA
 #' the user is asked.
 #'
 #' @importFrom madrat getConfig
 #' @importFrom withr local_dir
 #' @export
-runInRenv <- function(useSbatch = NA) {
+runInRenv <- function(renvInstallPackages = NULL, useSbatch = NA) {
   if (is.na(useSbatch)) {
     if (Sys.which("sbatch") == "") {
       useSbatch <- FALSE
@@ -35,7 +37,7 @@ runInRenv <- function(useSbatch = NA) {
   }, list(runFolder = runFolder))
 
   # install right away, because installing requires internet connection which is not available when running via sbatch
-  runInNewRSession(function(runFolder) {
+  runInNewRSession(function(runFolder, renvInstallPackages) {
     # TODO remove this when newest foreign can be installed on cluster
     renv::install("foreign@0.8-76")
     dependencies <- renv::dependencies(runFolder, errors = "fatal")
@@ -44,6 +46,7 @@ runInRenv <- function(useSbatch = NA) {
 
     renv::install("mrremind")
 
+    renv::install(renvInstallPackages)
     renv::snapshot(type = "all")
 
     # initialize madrat config
@@ -54,7 +57,7 @@ runInRenv <- function(useSbatch = NA) {
                  environmentVariables = Sys.getenv(),
                  locale = Sys.getlocale()),
             file.path(runFolder, "optionsEnvironmentVariablesLocale.rds"))
-  }, list(runFolder = runFolder), renvProject = runFolder)
+  }, list(runFolder = runFolder, renvInstallPackages = renvInstallPackages), renvProject = runFolder)
 
   run(useSbatch = useSbatch, madratConfig = getOption("madrat_cfg"), runFolder = runFolder)
 }
