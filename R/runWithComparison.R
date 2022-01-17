3581acc39479f3fe2f6dbce38d8add55e41a12b7
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-01-17 17:28:06 +0100

    difftool argument

--- R/runWithComparison.R
+++ R/runWithComparison.R
@@ -9,15 +9,19 @@
 #' @param piktestsFolder A new folder is created in the given directory. In that folder two folders called "old"
 #' and "new" are created which contain the actual piktests runs.
 #' @param whatToRun A list of computation objects. See \code{\link{computations}}.
+#' @param diffTool One or more names of command line tools for comparing two text files. The first one that is found
+#' via `Sys.which` is used in the comparison shell script. If none is found falls back to "diff".
 #' @param ... Additional arguments passed to \code{\link{run}}.
 #' @return Invisibly, the path to the folder holding the two actual piktests runs.
 #'
 #' @author Pascal Führlich
 #' @seealso \code{\link{run}}
 #'
+#' @importFrom utils head
 #' @export
 runWithComparison <- function(renvInstallPackages, piktestsFolder = getwd(),
-                              whatToRun = computations[c("magpiePreprocessing", "remindPreprocessing")], ...) {
+                              whatToRun = computations[c("magpiePreprocessing", "remindPreprocessing")],
+                              diffTool = c("delta", "colordiff", "diff"), ...) {
   stopifnot(!is.null(renvInstallPackages))
   now <- format(Sys.time(), "%Y_%m_%d-%H_%M")
   runFolder <- normalizePath(file.path(piktestsFolder, now), mustWork = FALSE)
@@ -29,7 +33,7 @@ runWithComparison <- function(renvInstallPackages, piktestsFolder = getwd(),
   run(renvInstallPackages, whatToRun = whatToRun, ..., runFolder = file.path(runFolder, paste0(now, "-new")))
 
   # on the cluster default diff does not support colors, so using pascal's delta (fancy diff tool) installation
-  diffTool <- if (file.exists("/home/pascalfu/.cargo/bin/delta")) "/home/pascalfu/.cargo/bin/delta" else "diff" # nolint
+  diffTool <- if (any(Sys.which(diffTool) != "")) head(diffTool[Sys.which(diffTool) != ""], 1) else "diff"
   for (computationName in names(whatToRun)) {
     compareLogsPath <- file.path(runFolder, paste0("compareLogs-", computationName, ".sh"))
     oldLog <- file.path(runFolder, paste0(now, "-old"), "computations", computationName,
